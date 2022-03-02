14e5cda06528dbae5320a64b2dd58c885995dde5
NightOwl <steelman2019e@gmail.com>
2022-03-02 19:52:40 +0900

    chore: Emotion, CSS Props 세팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from "next";
+import { css } from "@emotion/react";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <main>
+      <button css={button}>TestButton</button>
+    </main>
+  );
 };
 
 export default Home;
+
+const button = css`
+  width: 120px;
+  height: 48px;
+  border-radius: 12px;
+  border: 1px solid #eee;
+  background-color: #fff;
+`;
