2b1145441860452a35b1757401a2b9840ab462b6
schatzen <gomieti@naver.com>
2022-03-22 14:28:24 +0900

    feat: 프로젝트 카드 태그 추가(#9)

--- src/domains/Project/components/ProjectCard/ProjectCard.stories.tsx
+++ src/domains/Project/components/ProjectCard/ProjectCard.stories.tsx
@@ -13,5 +13,15 @@ export const DeadlineActive: ComponentStory<typeof ProjectCard> = () => (
     title={"영카트 기반 홈페이지 리뉴얼 프로젝트"}
     priceCount={100}
     deadlineCount={1}
+    tags={[
+      {
+        label: "기업",
+        icon: "icon",
+      },
+      {
+        label: "외주",
+        icon: "icon",
+      },
+    ]}
   />
 );
--- src/domains/Project/components/ProjectCard/ProjectCard.tsx
+++ src/domains/Project/components/ProjectCard/ProjectCard.tsx
@@ -1,18 +1,30 @@
 import { Shape } from "@/themes/Shape";
 import { css } from "@emotion/react";
+import Chip from "@mui/material/Chip";
+import Image from "next/image";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ApartmentIcon from "@mui/icons-material/Apartment";
+
 import {
   Button,
   ButtonProps,
   Typography,
   TypographyProps,
+  ChipProps,
 } from "@mui/material";
 import { useProjectCard } from "./useProjectCard";
+
+export type tagsModel = {
+  label: string;
+  icon: string;
+};
 
 export type ProjectCardProps = {
   cardImage: string;
   title: string;
   priceCount: number;
   deadlineCount: number;
+  tags: tagsModel[];
 };
 
 export const ProjectCard = ({
@@ -20,53 +32,66 @@ export const ProjectCard = ({
   title,
   priceCount,
   deadlineCount,
+  tags,
 }: ProjectCardProps) => {
   const { price, deadline, onClick } = useProjectCard(
     priceCount,
     deadlineCount
   );
   return (
-    <RootButton onClick={onClick}>
+    <StyledButton onClick={onClick}>
       <article css={styles.inner}>
         <section css={styles.imgContainer}>
-          <img src={cardImage} alt="" css={styles.imgWrapper}></img>
+          <Image
+            src={cardImage}
+            alt=""
+            layout="fill"
+            css={styles.imgWrapper}
+          ></Image>
         </section>
         <section css={styles.contentContainer}>
           <Title>{title}</Title>
           <Price>{price.text}</Price>
           <Deadline>{deadline.text}</Deadline>
-          <div css={styles.tagsContainer}></div>
+          <div css={styles.tagsContainer}>
+            {tags.map((it, index) => (
+              <StyledChip
+                key={it.label + index}
+                icon={<ApartmentIcon />}
+                label={it.label}
+              />
+            ))}
+          </div>
         </section>
       </article>
-    </RootButton>
+    </StyledButton>
   );
 };
 
 const styles = {
   inner: css`
     width: 260px;
-    aspect-ratio: 3/4;
     display: flex;
     flex-direction: column;
+    position: "relative";
   `,
   imgContainer: css`
     width: 100%;
-    height: 65%;
+    aspect-ratio: 1/1;
   `,
 
   imgWrapper: css`
     width: 100%;
-    height: 100%;
+    aspect-ratio: 1/1;
     border-top-left-radius: 8px;
     border-top-right-radius: 8px;
   `,
   contentContainer: css`
     width: 100%;
-    height: 35%;
     display: flex;
     flex-direction: column;
     gap: 4px;
-    padding: 16px;
+    padding: 12px;
   `,
   tagsContainer: css`
     width: 100%;
@@ -75,7 +100,9 @@ const styles = {
   `,
 };
 
-const RootButton = (p: ButtonProps) => (
+const Root = () => {};
+
+const StyledButton = (p: ButtonProps) => (
   <Button
     sx={{
       padding: 0,
@@ -111,7 +138,19 @@ const Deadline = (p: TypographyProps) => (
     variant={"caption"}
     color={"text.primary"}
     fontWeight={700}
-    sx={{ opacity: 0.8 }}
+    sx={{ opacity: 0.8, marginBottom: "12px" }}
+    {...p}
+  />
+);
+
+const StyledChip = (p: ChipProps) => (
+  <Chip
+    size="small"
+    sx={{
+      padding: "4px",
+      fontSize: "8px",
+      fontWeight: "700",
+    }}
     {...p}
   />
 );
