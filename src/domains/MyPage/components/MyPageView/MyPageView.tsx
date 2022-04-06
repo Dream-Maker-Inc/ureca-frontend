84b7ed44f9778b5d39ca6a267ff5fb9f564cad8f
NightOwl <steelman2019e@gmail.com>
2022-04-06 11:00:13 +0900

    refactor: MyPageView 하드코딩 데이터 분리

--- src/domains/MyPage/components/MyPageView/MyPageView.tsx
+++ src/domains/MyPage/components/MyPageView/MyPageView.tsx
@@ -9,6 +9,46 @@ import { MyPageIcon } from "../MyPageIcon";
 import { MyPageInfo } from "../MyPageInfo";
 
 export const MyPageView = () => {
+  const myPageIconData = {
+    avatarImage:
+      "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
+    brandName: "드림메이커",
+    brandInfo: "등록된 한 줄 소개가 없습니다.",
+  };
+
+  const basicInfoData = {
+    username: "한*현",
+    phone: "010 **** 7642",
+    email: "***@dremaker.biz",
+    bankname: "IBK기업은행",
+    accountNum: "12116958001010",
+    accountHolder: "주식회사 드림메이커",
+  };
+
+  const introduceSelfData = `
+  Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+  `;
+
+  const notificationData = {
+    projectRegistrationInfo: "받기",
+    projectProgressInfo: "받기",
+    noticeInfo: "받기",
+    marketingInfo: "받기",
+  };
+
+  const handleEditInfo = () => {
+    alert("edit info");
+  };
+
   return (
     <div css={styles.root}>
       <div css={styles.container}>
@@ -19,13 +59,13 @@ export const MyPageView = () => {
         <section css={styles.topContainer}>
           <div css={styles.topWrapper}>
             <TopTitle>프로필 관리</TopTitle>
-            <InfoEditBtn>정보 수정하기</InfoEditBtn>
+            <InfoEditBtn onClick={handleEditInfo}>정보 수정하기</InfoEditBtn>
           </div>
 
           <MyPageIcon
-            avatarImage="https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/"
-            brandNameText="드림메이커"
-            brandInfoText="등록된 한 줄 소개가 없습니다."
+            avatarImage={myPageIconData.avatarImage}
+            brandNameText={myPageIconData.brandName}
+            brandInfoText={myPageIconData.brandInfo}
           />
         </section>
 
@@ -35,14 +75,14 @@ export const MyPageView = () => {
 
             <div css={styles.innerContainer}>
               <div css={styles.innerLeftWrapper}>
-                <MyPageInfo title="실명" desc="한*현" />
-                <MyPageInfo title="휴대폰 번호" desc="010 **** 7642" />
-                <MyPageInfo title="이메일" desc="***@dremaker.biz" />
+                <MyPageInfo title="실명" desc={basicInfoData.username} />
+                <MyPageInfo title="휴대폰 번호" desc={basicInfoData.phone} />
+                <MyPageInfo title="이메일" desc={basicInfoData.email} />
               </div>
               <div css={styles.innerRightWrapper}>
-                <MyPageInfo title="은행명" desc="IBK기업은행" />
-                <MyPageInfo title="계좌 번호" desc="12116958001010" />
-                <MyPageInfo title="예금주" desc="주식회사 드림메이커" />
+                <MyPageInfo title="은행명" desc={basicInfoData.bankname} />
+                <MyPageInfo title="계좌 번호" desc={basicInfoData.accountNum} />
+                <MyPageInfo title="예금주" desc={basicInfoData.accountHolder} />
               </div>
             </div>
           </section>
@@ -51,18 +91,7 @@ export const MyPageView = () => {
 
           <section css={styles.innerTopContainer}>
             <InnerTitle>상세 소개</InnerTitle>
-            <ContentText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industrys standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </ContentText>
+            <ContentText>{introduceSelfData}</ContentText>
           </section>
 
           <hr css={styles.hr} />
@@ -71,12 +100,24 @@ export const MyPageView = () => {
             <InnerTitle>알림 정보 (알림톡)</InnerTitle>
             <div css={styles.innerContainer}>
               <div css={styles.innerLeftWrapper}>
-                <MyPageInfo title="프로젝트 등록 정보" desc="받기" />
-                <MyPageInfo title="프로젝트 진행 및 계약 정보" desc="받기" />
+                <MyPageInfo
+                  title="프로젝트 등록 정보"
+                  desc={notificationData.projectRegistrationInfo}
+                />
+                <MyPageInfo
+                  title="프로젝트 진행 및 계약 정보"
+                  desc={notificationData.projectProgressInfo}
+                />
               </div>
               <div css={styles.innerRightWrapper}>
-                <MyPageInfo title="공지사항 및 주요 정보" desc="받기" />
-                <MyPageInfo title="마케팅 및 홍보 메시지" desc="받지 않기" />
+                <MyPageInfo
+                  title="공지사항 및 주요 정보"
+                  desc={notificationData.noticeInfo}
+                />
+                <MyPageInfo
+                  title="마케팅 및 홍보 메시지"
+                  desc={notificationData.marketingInfo}
+                />
               </div>
             </div>
           </section>
