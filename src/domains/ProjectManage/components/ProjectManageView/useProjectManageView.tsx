5f341c5e09fb938c10aa84a44be98a6de10f6a41
schatzen <gomieti@naver.com>
2022-04-18 23:05:41 +0900

    feat: statusType 추가 및 view에 적용 (#30)

--- src/domains/ProjectManage/components/ProjectManageView/useProjectManageView.tsx
+++ src/domains/ProjectManage/components/ProjectManageView/useProjectManageView.tsx
@@ -2,6 +2,7 @@ import { SelectChangeEvent } from "@mui/material";
 import { useState } from "react";
 import { FilterType } from "../../type/FilterType";
 import { ProjectType } from "../../type/ProjectType";
+import { StatusType } from "../../type/StatusType";
 import { ProjectListItemProps } from "../ProjectListItem";
 
 export const useProjectManageView = () => {
@@ -36,7 +37,37 @@ export const useProjectManageView = () => {
           "반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
         transactionDate: "22.03.14 ~",
         price: "100,000원",
-        state: "진행중 ●",
+        state: StatusType.COMPLETED,
+      },
+    },
+    {
+      avatar: {
+        imgUrl:
+          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
+        brandName: "드림메이커",
+      },
+      project: {
+        id: "213456789",
+        title:
+          "반응형지 리뉴얼얼 개형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
+        transactionDate: "22.03.14 ~",
+        price: "100,000원",
+        state: StatusType.COMPLETED,
+      },
+    },
+    {
+      avatar: {
+        imgUrl:
+          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
+        brandName: "드림메이커",
+      },
+      project: {
+        id: "213456789",
+        title:
+          "반응개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
+        transactionDate: "22.03.14 ~",
+        price: "100,000원",
+        state: StatusType.CANCELD,
       },
     },
     {
@@ -51,7 +82,66 @@ export const useProjectManageView = () => {
           "반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
         transactionDate: "22.03.14 ~",
         price: "100,000원",
-        state: "진행중 ●",
+        state: StatusType.COMPLETED,
+      },
+    },
+    {
+      avatar: {
+        imgUrl:
+          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
+        brandName: "드림메이커",
+      },
+      project: {
+        id: "213456789",
+        title:
+          "홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
+        transactionDate: "22.03.14 ~",
+        price: "100,000원",
+        state: StatusType.PROCESSING,
+      },
+    },
+    {
+      avatar: {
+        imgUrl:
+          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
+        brandName: "드림메이커",
+      },
+      project: {
+        id: "213456789",
+        title:
+          "반응형 홈페이지형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
+        transactionDate: "22.03.14 ~",
+        price: "100,000원",
+        state: StatusType.PROCESSING,
+      },
+    },
+    {
+      avatar: {
+        imgUrl:
+          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
+        brandName: "드림메이커",
+      },
+      project: {
+        id: "213456789",
+        title: "반응형 홈페이지 리뉴얼",
+        transactionDate: "22.03.14 ~",
+        price: "100,000원",
+        state: StatusType.COMPLETED,
+      },
+    },
+    {
+      avatar: {
+        imgUrl:
+          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
+        brandName: "드림메이커",
+      },
+      project: {
+        id: "213456789",
+        title:
+          "반응형 홈페이지 리뉴얼 이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
+        transactionDate: "22.03.14 ~",
+        price: "100,000원",
+        state: StatusType.CANCELD,
       },
     },
     {
@@ -66,7 +156,7 @@ export const useProjectManageView = () => {
           "반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
         transactionDate: "22.03.14 ~",
         price: "100,000원",
-        state: "진행중 ●",
+        state: StatusType.PROCESSING,
       },
     },
     {
@@ -78,100 +168,10 @@ export const useProjectManageView = () => {
       project: {
         id: "213456789",
         title:
-          "반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
+          "반응형 홈페이지 지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
         transactionDate: "22.03.14 ~",
         price: "100,000원",
-        state: "진행중 ●",
-      },
-    },
-    {
-      avatar: {
-        imgUrl:
-          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
-        brandName: "드림메이커",
-      },
-      project: {
-        id: "213456789",
-        title:
-          "반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
-        transactionDate: "22.03.14 ~",
-        price: "100,000원",
-        state: "진행중 ●",
-      },
-    },
-    {
-      avatar: {
-        imgUrl:
-          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
-        brandName: "드림메이커",
-      },
-      project: {
-        id: "213456789",
-        title:
-          "반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
-        transactionDate: "22.03.14 ~",
-        price: "100,000원",
-        state: "진행중 ●",
-      },
-    },
-    {
-      avatar: {
-        imgUrl:
-          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
-        brandName: "드림메이커",
-      },
-      project: {
-        id: "213456789",
-        title:
-          "반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
-        transactionDate: "22.03.14 ~",
-        price: "100,000원",
-        state: "진행중 ●",
-      },
-    },
-    {
-      avatar: {
-        imgUrl:
-          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
-        brandName: "드림메이커",
-      },
-      project: {
-        id: "213456789",
-        title:
-          "반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
-        transactionDate: "22.03.14 ~",
-        price: "100,000원",
-        state: "진행중 ●",
-      },
-    },
-    {
-      avatar: {
-        imgUrl:
-          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
-        brandName: "드림메이커",
-      },
-      project: {
-        id: "213456789",
-        title:
-          "반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
-        transactionDate: "22.03.14 ~",
-        price: "100,000원",
-        state: "진행중 ●",
-      },
-    },
-    {
-      avatar: {
-        imgUrl:
-          "https://images.unsplash.com/photo-1601158935942-52255782d322?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80/",
-        brandName: "드림메이커",
-      },
-      project: {
-        id: "213456789",
-        title:
-          "반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼 개발운영 중 반응형 홈페이지 리뉴얼",
-        transactionDate: "22.03.14 ~",
-        price: "100,000원",
-        state: "진행중 ●",
+        state: StatusType.COMPLETED,
       },
     },
   ];
