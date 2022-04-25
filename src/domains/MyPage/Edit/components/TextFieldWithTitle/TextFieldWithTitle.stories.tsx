f91293c561b6da1d8fa3921baf5bc5f1720ac3a6
schatzen <gomieti@naver.com>
2022-04-25 18:32:29 +0900

    feat: TextField 컴포넌트 구현 (#29)

---
+++ src/domains/MyPage/Edit/components/TextFieldWithTitle/TextFieldWithTitle.stories.tsx
@@ -0,0 +1,12 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import {TextFieldWithTitle} from "./TextFieldWithTitle";
+
+const Meta: ComponentMeta<typeof TextFieldWithTitle> = {
+    title: "MyPage/Edit TextField",
+    component: TextFieldWithTitle,
+};
+export default Meta;
+
+export const Icon: ComponentStory<typeof TextFieldWithTitle> = () => (
+    <TextFieldWithTitle title="닉네임" desc="전문가 리스트에 표시되는 닉네임을 입력해 주세요." placeholder = "한 줄 소개를 입력하세요." />
+);
